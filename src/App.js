ab2daa043f8dccfa94ee7c2d66f5b7eff212d938
Code2Riches <105663804+Code2Riches@users.noreply.github.com>
2022-11-22 15:01:04 -0500

    Initial Shop Cart commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Navbar from "./components/Navbar";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { useState, useEffect } from "react";
+import CartPage from "./pages/CartPage";
+
 function App() {
   return (
     <>
       <Navbar />
       <h1>Final Project</h1>
+
+      <CartPage />
     </>
   );
 }

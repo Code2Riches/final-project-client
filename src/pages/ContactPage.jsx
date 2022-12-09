d2f0e41e63800ce1c9d523a9651d0635281b9dde
ClaytonStealth <claytonses@yahoo.com>
2022-12-09 14:59:22 -0500

    Update ContactPage.jsx

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -5,7 +5,7 @@ import mutantSoda from "../assets/mutant-soda.png";
 
 const Support = () => {
   return (
-    <div name='support' className='w-full my-18'>
+    <div name='support' className='w-full mt-16 pb-12'>
       <div className='w-full h-[700px] bg-gray-900/90 absolute '>
         <img
           src={mutantSoda}
